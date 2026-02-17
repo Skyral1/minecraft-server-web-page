3d56e62a232151b628a89363ffaf18baa0e5a5e7
Skyral <thibault.soquette.pro@gmail.com>
2026-02-17 23:45:21 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="icon" type="image/png" sizes="96x96" href="favicon.png">
+	<title><?=$_SERVER['SERVER_NAME'];?> 👑 </title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="WebStrator, votre hébergeur web par excellence.">
+	<meta name="author" content="OctoGency SAS">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,700">
+	<style type="text/css">
+	html {
+		height: 100%;
+	}
+	body {
+		background-image: linear-gradient(-20deg, #dcb0ed 0%, #99c99c 100%);
+		background-repeat: no-repeat;
+		background-position: center;
+		background-size: cover;
+		width: 100%;
+		height: 100%;
+		font-family: Raleway;
+	}
+</style>
+</head>
+<body>
+	<br>
+	<div class="container">
+		<img src="https://minestrator.com/assets/img/minestrator-new-2021.png" style="display: block;margin: 0 auto; max-width: 580px;">
+		<br>
+		<div class="card">
+			<div class="card-header" style="text-align: center;font-size: 25px;">Votre site <b><?=$_SERVER['SERVER_NAME'];?></b> est <b>fonctionnel</b> ! <i class="fa fa-check" aria-hidden="true"></i></div>
+			<div class="card-body" style="text-align: center;font-size: 22px;">
+				<img src="internet.gif" style="max-width: 100%;"><br>
+				<br>
+				Vous pouvez maintenant gérer votre site à partir de l'interface de gestion de la MyBox associée 🤗<br>
+				<hr>
+				<a href="https://minestrator.com/panel/gerer/mes/serveurs" class="btn btn-success btn-block btn-lg">Accéder à l'interface de gestion</a>
+			</div>
+		</div>
+		<hr>
+		<div class="card">
+			<div class="card-body">
+				<center>Copyright 2017-<?=date('Y');?> © <a href="https://minestrator.com">MineStrator.com</a> est un service édité par <a href="https://octogency.com">OctoGency</a></center>
+			</div>
+		</div>
+	</div>
+
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+</html>
